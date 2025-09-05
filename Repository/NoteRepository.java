57f7a7b9b13b66bfc80437a9c9e98140f9d08e95
Mukulpatidar <patidarmukul127.com>
2025-09-05 13:45:09 +0530

    Fix: Restructure project to standard Maven layout

--- Repository/NoteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.notesapp.Repository;
-
-import com.example.notesapp.Model.*;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NoteRepository extends JpaRepository<Note, Long> {
-}
